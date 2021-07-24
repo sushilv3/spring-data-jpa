2f2c6304863d4089d0bc8931c5e3e11795ee23b6
sushilv3 <sushilv3rma@gmail.com>
2021-07-24 08:19:21 +0530

    spring data jpa poc

---
+++ src/test/java/com/kodecamp/springdatajpa/PracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kodecamp.springdatajpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
